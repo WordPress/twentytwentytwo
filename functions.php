167d268f36b4267e0ab3dbe9d63c9fcda783cec1
Jeff Ong <jonger4@gmail.com>
2021-10-04 12:17:36 -0400

    Declare fonts and load them in editor + view.

--- functions.php
+++ functions.php
@@ -14,9 +14,6 @@ if ( ! function_exists( 'twentytwentytwo_support' ) ) :
 
 		// Add support for editor styles.
 		add_theme_support( 'editor-styles' );
-
-		// Enqueue editor styles.
-		add_editor_style( 'style.css' );
 	}
 	add_action( 'after_setup_theme', 'twentytwentytwo_support' );
 endif;
@@ -28,5 +25,18 @@ function twentytwentytwo_scripts() {
 	// Enqueue theme stylesheet.
 	wp_enqueue_style( 'twentytwentytwo-style', get_template_directory_uri() . '/style.css', array(), wp_get_theme()->get( 'Version' ) );
 }
+
+/**
+ * Enqueue editor styles.
+ */
+function twentytwentytwo_editor_styles() {
+	// Enqueue editor styles.
+	add_editor_style(
+		array(
+			'https://fonts.googleapis.com/css2?family=Source+Serif+Pro:ital,wght@0,300;0,400;0,700;1,300;1,400;1,700&display=swap',
+		)
+	);
+}
+add_action( 'admin_init', 'twentytwentytwo_editor_styles' );
 
 add_action( 'wp_enqueue_scripts', 'twentytwentytwo_scripts' );

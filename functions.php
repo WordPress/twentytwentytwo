345bd403e2a32b52a0ae103067aa4676dc2bc12d
Jeff Ong <jonger4@gmail.com>
2021-10-04 15:35:19 -0400

    Declare fonts and load them in editor + view (#3)
    * Declare fonts and load them in editor + view.
    
    * Add name and reorder keys.
    
    * Bundle Source Serif Pro.
    
    * Use specific font weights instead of named.
    
    * Use relative path for fonts.
    
    * Move font loading to main stylesheet and use absolute path to locate font files.
    
    * Remove unneeded editor-style support call and cleanup actions.
    
    Co-authored-by: Kjell Reigstad <kjell@kjellr.com>

--- functions.php
+++ functions.php
@@ -11,12 +11,6 @@ if ( ! function_exists( 'twentytwentytwo_support' ) ) :
 
 		// Adding support for responsive embedded content.
 		add_theme_support( 'responsive-embeds' );
-
-		// Add support for editor styles.
-		add_theme_support( 'editor-styles' );
-
-		// Enqueue editor styles.
-		add_editor_style( 'style.css' );
 	}
 	add_action( 'after_setup_theme', 'twentytwentytwo_support' );
 endif;
@@ -28,5 +22,17 @@ function twentytwentytwo_scripts() {
 	// Enqueue theme stylesheet.
 	wp_enqueue_style( 'twentytwentytwo-style', get_template_directory_uri() . '/style.css', array(), wp_get_theme()->get( 'Version' ) );
 }
-
 add_action( 'wp_enqueue_scripts', 'twentytwentytwo_scripts' );
+
+/**
+ * Enqueue editor styles.
+ */
+function twentytwentytwo_editor_styles() {
+	// Enqueue editor styles.
+	add_editor_style(
+		array(
+			get_stylesheet_uri(),
+		)
+	);
+}
+add_action( 'admin_init', 'twentytwentytwo_editor_styles' );

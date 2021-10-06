44531d89d9c0cd85a0af72d7d0142911ddbfcea9
AbmSourav <keramotul.islam@gmail.com>
2021-10-07 00:57:18 +0600

    functions duplicate checks added (#29)

--- functions.php
+++ functions.php
@@ -15,24 +15,28 @@ if ( ! function_exists( 'twentytwentytwo_support' ) ) :
 	add_action( 'after_setup_theme', 'twentytwentytwo_support' );
 endif;
 
-/**
- * Enqueue scripts and styles.
- */
-function twentytwentytwo_scripts() {
-	// Enqueue theme stylesheet.
-	wp_enqueue_style( 'twentytwentytwo-style', get_template_directory_uri() . '/style.css', array(), wp_get_theme()->get( 'Version' ) );
-}
-add_action( 'wp_enqueue_scripts', 'twentytwentytwo_scripts' );
+if ( ! function_exists( 'twentytwentytwo_scripts' ) ) :
+	/**
+	 * Enqueue scripts and styles.
+	 */
+	function twentytwentytwo_scripts() {
+		// Enqueue theme stylesheet.
+		wp_enqueue_style( 'twentytwentytwo-style', get_template_directory_uri() . '/style.css', array(), wp_get_theme()->get( 'Version' ) );
+	}
+	add_action( 'wp_enqueue_scripts', 'twentytwentytwo_scripts' );
+endif;
 
-/**
- * Enqueue editor styles.
- */
-function twentytwentytwo_editor_styles() {
-	// Enqueue editor styles.
-	add_editor_style(
-		array(
-			get_stylesheet_uri(),
-		)
-	);
-}
-add_action( 'admin_init', 'twentytwentytwo_editor_styles' );
+if ( ! function_exists( 'twentytwentytwo_editor_styles' ) ) :
+	/**
+	 * Enqueue editor styles.
+	 */
+	function twentytwentytwo_editor_styles() {
+		// Enqueue editor styles.
+		add_editor_style(
+			array(
+				get_stylesheet_uri(),
+			)
+		);
+	}
+	add_action( 'admin_init', 'twentytwentytwo_editor_styles' );
+endif;

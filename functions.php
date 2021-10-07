0db038ab4bcda42f0b07095e2fee5c04a20120dd
Niels Lange <info@nielslange.de>
2021-10-07 16:32:05 +0700

    Add theme support for editor styles

--- functions.php
+++ functions.php
@@ -11,6 +11,9 @@ if ( ! function_exists( 'twentytwentytwo_support' ) ) :
 
 		// Adding support for responsive embedded content.
 		add_theme_support( 'responsive-embeds' );
+
+		// Adding support for editor styles.
+		add_theme_support( 'editor-styles');
 	}
 	add_action( 'after_setup_theme', 'twentytwentytwo_support' );
 endif;
@@ -22,4 +25,17 @@ function twentytwentytwo_scripts() {
 	// Enqueue theme stylesheet.
 	wp_enqueue_style( 'twentytwentytwo-style', get_template_directory_uri() . '/style.css', array(), wp_get_theme()->get( 'Version' ) );
 }
-add_action( 'enqueue_block_assets', 'twentytwentytwo_scripts' );
+add_action( 'wp_enqueue_scripts', 'twentytwentytwo_scripts' );
+
+/**
+ * Enqueue editor styles.
+ */
+function twentytwentytwo_editor_styles() {
+	// Enqueue editor styles.
+	add_editor_style(
+		array(
+			get_stylesheet_uri(),
+		)
+	);
+}
+add_action( 'admin_init', 'twentytwentytwo_editor_styles' );

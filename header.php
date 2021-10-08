5ff0b1e15565ba6a5e64ab2f288ed5618f815cb3
Saju Ahmed <59993047+Riyadh1734@users.noreply.github.com>
2021-10-08 20:09:30 +0600

    header.php file added

---
+++ header.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * The header.
+ *
+ * This is the template that displays all of the <head> section and everything up until main.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package WordPress
+ * @subpackage Twenty_Twenty_two
+ * @since Twenty Twenty-two 0.1
+ */
+
+?>

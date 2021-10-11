62a610d013f3f1d3624b07d91c11c57013b895bd
Kjell Reigstad <kjell@kjellr.com>
2021-10-11 16:06:05 -0400

    Add footer patterns.

---
+++ inc/patterns/footer-navigation.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Footer with navigation and citation
+ */
+return array(
+	'title'      => __( 'Footer with navigation and citation', 'twentytwentytwo' ),
+	'categories' => array( 'twentytwentytwo-footers' ),
+	'blockTypes' => array( 'core/template-part/footer' ),
+	'content'    => '<!-- wp:group {"align":"wide","style":{"spacing":{"padding":{"top":"4rem","bottom":"4rem"}}},"layout":{"type":"flex","justifyContent":"space-between"}} -->
+					<div class="wp-block-group alignwide" style="padding-top:4rem;padding-bottom:4rem"><!-- wp:navigation -->
+					<!-- wp:page-list {"isNavigationChild":true,"showSubmenuIcon":true,"openSubmenusOnClick":false} /-->
+					<!-- /wp:navigation -->
+
+					<!-- wp:paragraph {"align":"right"} -->
+					<p class="has-text-align-right">' . esc_html__( 'Proudly powered by ', 'twentytwentytwo' ) . '<a href="' . esc_url( 'https://wordpress.org' ) . '" rel="nofollow">WordPress</a></p>
+					<!-- /wp:paragraph --></div>
+					<!-- /wp:group -->',
+);

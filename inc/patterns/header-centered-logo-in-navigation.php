ec40e46bb26173e15d3af577f2d19b51a6f9b422
Kjell Reigstad <kjell@kjellr.com>
2021-10-11 13:07:59 -0400

    Use links instead of two page lists.

--- inc/patterns/header-centered-logo-in-navigation.php
+++ inc/patterns/header-centered-logo-in-navigation.php
@@ -6,13 +6,17 @@ return array(
 	'title'      => __( 'Header with centered Logo in Navigation', 'twentytwentytwo' ),
 	'categories' => array( 'twentytwentytwo-headers' ),
 	'blockTypes' => array( 'core/template-part/header' ),
-	'content'    => '<!-- wp:group {"align":"wide","tagName":"header","style":{"spacing":{"padding":{"bottom":"8rem","top":"max(1.25rem, 5vw)"}}},"layout":{"type":"flex","justifyContent":"center"}} -->
+	'content'    => '<!-- wp:group {"tagName":"header","align":"wide","style":{"spacing":{"padding":{"bottom":"8rem","top":"max(1.25rem, 5vw)"}}},"layout":{"type":"flex","justifyContent":"center"}} -->
 					<header class="wp-block-group alignwide" style="padding-top:max(1.25rem, 5vw);padding-bottom:8rem"><!-- wp:navigation {"itemsJustification":"right","isResponsive":true} -->
-					<!-- wp:page-list {"isNavigationChild":true,"showSubmenuIcon":true,"openSubmenusOnClick":false} /-->
+					<!-- wp:navigation-link {"isTopLevelLink":true} /-->
+
+					<!-- wp:navigation-link {"isTopLevelLink":true} /-->
 
 					<!-- wp:site-logo {"width":90} /-->
 
-					<!-- wp:page-list {"isNavigationChild":true,"showSubmenuIcon":true,"openSubmenusOnClick":false} /-->
+					<!-- wp:navigation-link {"isTopLevelLink":true} /-->
+
+					<!-- wp:navigation-link {"isTopLevelLink":true} /-->
 					<!-- /wp:navigation --></header>
 					<!-- /wp:group -->',
 );

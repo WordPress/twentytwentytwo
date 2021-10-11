abcddfaddd2410cd28759f4dfdd418050cf78ba6
Saju Ahmed <59993047+Riyadh1734@users.noreply.github.com>
2021-10-11 20:03:30 +0600

    updated index.php file

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * The main template file
+ *
+ * This is the most generic template file in a WordPress theme
+ * and one of the two required files for a theme (the other being style.css).
+ * It is used to display a page when nothing more specific matches a query.
+ * E.g., it puts together the home page when no home.php file exists.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package WordPress
+ * @subpackage Twenty_Twenty_Two
+ * @since Twenty Twenty-Two 0.1
+ */
+
+get_header(); ?> 

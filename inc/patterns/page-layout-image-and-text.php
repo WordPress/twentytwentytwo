be3f026d977bef2521de0d06c66af25a9642e1bb
Kjell Reigstad <kjell@kjellr.com>
2021-10-14 14:23:36 -0400

    Allow bird to go full-width

--- inc/patterns/page-layout-image-and-text.php
+++ inc/patterns/page-layout-image-and-text.php
@@ -5,15 +5,18 @@
 return array(
 	'title'      => __( 'Page layout with image and text', 'twentytwentytwo' ),
 	'categories' => array( 'twentytwentytwo-pages' ),
-	'content'    => '<!-- wp:group {"align":"wide","style":{"spacing":{"padding":{"top":"8rem","bottom":"8rem","right":"max(1.25rem, 5vw)","left":"max(1.25rem, 5vw)"}}}} -->
-	<div class="wp-block-group alignwide" style="padding-top:8rem;padding-bottom:8rem;padding-right:max(1.25rem, 5vw);padding-left:max(1.25rem, 5vw)"><!-- wp:heading {"align":"wide","style":{"typography":{"fontSize":"clamp(4rem, 8vw, 7.5rem)","lineHeight":"1.15","fontWeight":"300"}}} -->
+	'content'    => '<!-- wp:group {"align":"full","style":{"spacing":{"padding":{"top":"8rem","bottom":"2rem","right":"max(1.25rem, 5vw)","left":"max(1.25rem, 5vw)"}}},"layout":{"inherit":true}} -->
+<div class="wp-block-group alignfull" style="padding-top:8rem;padding-right:max(1.25rem, 5vw);padding-bottom:2rem;padding-left:max(1.25rem, 5vw)"><!-- wp:heading {"align":"wide","style":{"typography":{"fontSize":"clamp(4rem, 8vw, 7.5rem)","lineHeight":"1.15","fontWeight":"300"}}} -->
 	<h2 class="alignwide" style="font-size:clamp(4rem, 8vw, 7.5rem);font-weight:300;line-height:1.15">' . wp_kses_post( __( '<em>Watching Birds </em><br><em>in the Garden</em>', 'twentytwentytwo' ) ) . '</h2>
-	<!-- /wp:heading -->
+	<!-- /wp:heading --></div>
+	<!-- /wp:group -->
 	
 	<!-- wp:image {"align":"full","style":{"color":{}}} -->
 	<figure class="wp-block-image alignfull"><img src="' . esc_url( get_template_directory_uri() ) . '/assets/img/patterns/watching-birds.png" alt="' . esc_attr__( 'TBD', 'twentytwentytwo' ) . '"/></figure>
 	<!-- /wp:image -->
 	
+	<!-- wp:group {"align":"full","style":{"spacing":{"padding":{"top":"2rem","bottom":"8rem","right":"max(1.25rem, 5vw)","left":"max(1.25rem, 5vw)"}}},"layout":{"inherit":true}} -->
+	<div class="wp-block-group alignfull" style="padding-top:2rem;padding-right:max(1.25rem, 5vw);padding-bottom:8rem;padding-left:max(1.25rem, 5vw)">
 	<!-- wp:columns {"align":"wide"} -->
 	<div class="wp-block-columns alignwide"><!-- wp:column {"verticalAlignment":"bottom","style":{"spacing":{"padding":{"bottom":"1em"}}}} -->
 	<div class="wp-block-column is-vertically-aligned-bottom" style="padding-bottom:1em"><!-- wp:site-logo {"width":60} /--></div>

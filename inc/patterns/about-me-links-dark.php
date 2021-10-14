71a2e2daf1401879e1736a386c7fe3c27c317d19
Kjell Reigstad <kjell@kjellr.com>
2021-10-14 11:43:22 -0400

    Revise link in bio patterns.

--- inc/patterns/about-me-links-dark.php
+++ inc/patterns/about-me-links-dark.php
@@ -1,16 +1,16 @@
 <?php
 /**
- * About Me - Link in Bio - Dark
+ * About Me - Links
  */
 return array(
 	'title'      => __( 'About Me - Link in Bio - Dark', 'twentytwentytwo' ),
 	'categories' => array( 'twentytwentytwo-about-me' ),
-	'content'    => '<!-- wp:group {"align":"full","style":{"elements":{"link":{"color":{"text":"var:preset|color|background"}}},"spacing":{"padding":{"top":"10rem","right":"0rem","bottom":"10rem","left":"0rem"}}},"backgroundColor":"primary","textColor":"background","layout":{"inherit":true,"contentSize":"480px"}} -->
-					<div class="wp-block-group alignfull has-background-color has-primary-background-color has-text-color has-background has-link-color" style="padding-top:10rem;padding-right:0rem;padding-bottom:10rem;padding-left:0rem"><!-- wp:group -->
+	'content'    => '<!-- wp:group {"align":"full","style":{"elements":{"link":{"color":{"text":"var:preset|color|background"}}},"spacing":{"padding":{"top":"10rem","right":"max(1.25rem, 5vw)","bottom":"10rem","left":"max(1.25rem, 5vw)"}}},"backgroundColor":"primary","textColor":"background","layout":{"inherit":false,"contentSize":"400px"}} -->
+					<div class="wp-block-group alignfull has-background-color has-primary-background-color has-text-color has-background has-link-color" style="padding-top:10rem;padding-right:max(1.25rem, 5vw);padding-bottom:10rem;padding-left:max(1.25rem, 5vw)"><!-- wp:group -->
 					<div class="wp-block-group">
 
 					<!-- wp:image {"width":100,"height":100,"sizeSlug":"full","linkDestination":"none","className":"is-style-rounded"} -->
-					<figure class="wp-block-image size-full is-resized is-style-rounded"><img src="' . esc_url( get_stylesheet_directory_uri() ) . '/assets/img/patterns/swallow.png" alt="" width="100" height="100"/></figure>
+					<figure class="wp-block-image size-full is-resized is-style-rounded"><img src="' . esc_url( get_stylesheet_directory_uri() ) . '/assets/img/patterns/swallow.png" alt="' . esc_attr__( 'Logo featuring a flying bird', 'twentytwentytwo' ) . '" width="100" height="100"/></figure>
 					<!-- /wp:image -->
 
 					<!-- wp:heading {"textAlign":"left","fontSize":"huge"} -->

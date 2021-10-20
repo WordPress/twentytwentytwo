d03d4c1f538e80816deadbcdf2c9daddd9878695
Kjell Reigstad <kjell@kjellr.com>
2021-10-20 15:22:21 -0400

    Clean up block markup.

--- inc/patterns/general-image-with-caption.php
+++ inc/patterns/general-image-with-caption.php
@@ -6,9 +6,8 @@ return array(
 	'title'      => __( 'Image with caption', 'twentytwentytwo' ),
 	'categories' => array( 'twentytwentytwo-general' ),
 	'content'    => '<!-- wp:group {"align":"full","style":{"spacing":{"padding":{"top":"6rem","right":"1rem","bottom":"6rem","left":"1rem"}},"elements":{"link":{"color":{"text":"var:preset|color|background"}}}},"backgroundColor":"primary","textColor":"background","layout":{"inherit":true}} -->
-					<div class="wp-block-group alignfull has-background-color has-primary-background-color has-text-color has-background has-link-color" style="padding-top:6rem;padding-right:1rem;padding-bottom:6rem;padding-left:1rem"><!-- wp:group -->
-					<div class="wp-block-group"><!-- wp:media-text {"mediaId":202,"mediaLink":"' . esc_url( get_template_directory_uri() ) . '/assets/img/patterns/hummingbird-tall.jpg","mediaType":"image","verticalAlignment":"bottom","imageFill":false} -->
-					<div class="wp-block-media-text alignwide is-stacked-on-mobile is-vertically-aligned-bottom"><figure class="wp-block-media-text__media"><img src="' . esc_url( get_template_directory_uri() ) . '/assets/img/patterns/hummingbird-tall.jpg" alt="' . esc_attr__( 'Hummingbird illustration', 'twentytwentytwo' ) . '" class="size-full"/></figure><div class="wp-block-media-text__content"><!-- wp:paragraph -->
+					<div class="wp-block-group alignfull has-background-color has-primary-background-color has-text-color has-background has-link-color" style="padding-top:6rem;padding-right:1rem;padding-bottom:6rem;padding-left:1rem"><!-- wp:media-text {"mediaId":202,"mediaLink":"' . esc_url( get_template_directory_uri() ) . '/assets/img/patterns/hummingbird-tall.jpg","mediaType":"image","verticalAlignment":"bottom","imageFill":false} -->
+					<div class="wp-block-media-text alignwide is-stacked-on-mobile is-vertically-aligned-bottom"><figure class="wp-block-media-text__media"><img src="' . esc_url( get_template_directory_uri() ) . '/assets/img/patterns/hummingbird-tall.jpg" alt="' . esc_attr__( 'Hummingbird illustration', 'twentytwentytwo' ) . '" class="wp-image-202 size-full"/></figure><div class="wp-block-media-text__content"><!-- wp:paragraph -->
 					<p><strong>' . esc_html__( 'Hummingbird', 'twentytwentytwo' ) . '</strong></p>
 					<!-- /wp:paragraph -->
 
@@ -16,6 +15,5 @@ return array(
 					<p>' . esc_html__( 'A beautiful bird featuring a surprising set of color feathers.', 'twentytwentytwo' ) . '</p>
 					<!-- /wp:paragraph --></div></div>
 					<!-- /wp:media-text --></div>
-					<!-- /wp:group --></div>
 					<!-- /wp:group -->',
 );

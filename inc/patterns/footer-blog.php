c4d5fcf2b5512dbf4bfafb7949bd9b8770229017
Milan Dinić <milan@srpski.biz>
2021-10-27 16:17:25 +0200

    I18n: change broken strings into string with placeholder.
    See #124.

--- inc/patterns/footer-blog.php
+++ inc/patterns/footer-blog.php
@@ -43,7 +43,12 @@ return array(
 					<div class="wp-block-group alignwide" style="padding-top:4rem;padding-bottom:4rem"><!-- wp:site-title /-->
 
 					<!-- wp:paragraph {"align":"right"} -->
-					<p class="has-text-align-right">' . esc_html__( 'Proudly powered by ', 'twentytwentytwo' ) . '<a href="' . esc_url( __( 'https://wordpress.org', 'twentytwentytwo' ) ) . '" rel="nofollow">WordPress</a></p>
+					<p class="has-text-align-right">' .
+					sprintf(
+						/* Translators: WordPress link. */
+						esc_html__( 'Proudly powered by %s', 'twentytwentytwo' ),
+						'<a href="' . esc_url( __( 'https://wordpress.org', 'twentytwentytwo' ) ) . '" rel="nofollow">WordPress</a>'
+					) . '</p>
 					<!-- /wp:paragraph --></div>
 					<!-- /wp:group --></div>
 					<!-- /wp:group -->',

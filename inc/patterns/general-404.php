e68c422b4a216bfb348b9391bd76fe0f5b6aed3b
Jeff Ong <jonger4@gmail.com>
2021-10-29 14:12:42 -0400

    Remove margins.

--- inc/patterns/general-404.php
+++ inc/patterns/general-404.php
@@ -5,8 +5,8 @@
 return array(
 	'title'      => __( '404 content', 'twentytwentytwo' ),
 	'categories' => array( 'twentytwentytwo-general' ),
-	'content'    => '<!-- wp:heading {"style":{"typography":{"fontSize":"clamp(4rem, 40vw, 20rem)","fontWeight":"200","lineHeight":"1"}},"className":"has-text-align-center"} -->
-					<h2 class="has-text-align-center" style="font-size:clamp(4rem, 40vw, 20rem);font-weight:200;line-height:1">404</h2>
+	'content'    => '<!-- wp:heading {"style":{"typography":{"fontSize":"clamp(4rem, 40vw, 20rem)","fontWeight":"200","lineHeight":"1","spacing":{"margin":{"top":"0","bottom":"0"}}}},"className":"has-text-align-center"} -->
+					<h2 class="has-text-align-center" style="font-size:clamp(4rem, 40vw, 20rem);font-weight:200;line-height:1;margin-top:0;margin-bottom:0;">404</h2>
 					<!-- /wp:heading -->
 					<!-- wp:paragraph {"align":"center"} -->
 					<p class="has-text-align-center">' . esc_html( 'This page could not be found. Maybe try a search?', 'twentytwentytwo' ) . '</p>

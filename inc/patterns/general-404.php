66c8adef89d4a58dbee046554a016667c12719f2
Jeff Ong <jonger4@gmail.com>
2021-10-29 13:57:55 -0400

    Improve 404 message.
    Co-authored-by: Saju Ahmed <59993047+Riyadh1734@users.noreply.github.com>

--- inc/patterns/general-404.php
+++ inc/patterns/general-404.php
@@ -9,7 +9,7 @@ return array(
 					<h2 class="has-text-align-center" style="font-size:clamp(4rem, 40vw, 20rem);font-weight:200;line-height:1">404</h2>
 					<!-- /wp:heading -->
 					<!-- wp:paragraph {"align":"center"} -->
-					<p class="has-text-align-center">' . esc_html( 'This page could not be found.', 'twentytwentytwo' ) . '</p>
+					<p class="has-text-align-center">' . esc_html( 'This page could not be found. Maybe try a search?', 'twentytwentytwo' ) . '</p>
 					<!-- /wp:paragraph -->
 					<!-- wp:search {"label":"Search","showLabel":false,"width":50,"widthUnit":"%","buttonText":"Search","buttonUseIcon":true,"align":"center"} /-->
 					<!-- wp:spacer {"height":128} -->

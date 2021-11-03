4af172262438047f05a86a2ef6c8234b01d37d8a
Jeff Ong <jonger4@gmail.com>
2021-11-03 10:59:33 -0400

    Link variables to global styles and apply them to search and file blocks.

--- functions.php
+++ functions.php
@@ -45,6 +45,9 @@ if ( ! function_exists( 'twentytwentytwo_styles' ) ) :
 		// Add styles inline.
 		wp_add_inline_style( 'twentytwentytwo-style', twentytwentytwo_get_font_face_styles() );
 
+		// Add global styles linked variables.
+		wp_add_inline_style( 'twentytwentytwo-style', twentytwentytwo_theme_variables() );
+
 		// Add metadata to the CSS stylesheet.
 		wp_style_add_data( 'twentytwentytwo-style', 'path', get_template_directory() . '/style.css' );
 
@@ -56,6 +59,22 @@ if ( ! function_exists( 'twentytwentytwo_styles' ) ) :
 
 endif;
 
+/**
+ * Create variables linked to global styles.
+ *
+ * @return string
+ */
+function twentytwentytwo_theme_variables() {
+	if ( ! function_exists( 'gutenberg_get_global_styles' ) ) {
+		return;
+	}
+
+	// Get the properties of the button that we want to use in other blocks like search and file.
+	$button_styles = gutenberg_get_global_styles( array(), 'core/button' );
+
+	// Create variables that point to the value of those customizable properties.
+	return ':root, body { --wp--theme--button--color--text: ' . $button_styles['color']['text'] . '; --wp--theme--button--color--background: ' . $button_styles['color']['background'] . '; --wp--theme--button--border--radius: ' . $button_styles['border']['radius'] . '; }';
+}
 
 if ( ! function_exists( 'twentytwentytwo_editor_styles' ) ) :
 
@@ -68,6 +87,12 @@ if ( ! function_exists( 'twentytwentytwo_editor_styles' ) ) :
 
 		// Add styles inline.
 		wp_add_inline_style( 'wp-block-library', twentytwentytwo_get_font_face_styles() );
+
+		// Add global styles linked variables.
+		wp_add_inline_style( 'wp-block-library', twentytwentytwo_theme_variables() );
+
+		// Add stylesheet.
+		add_editor_style( array( get_stylesheet_uri() ) );
 
 	}
 	add_action( 'admin_init', 'twentytwentytwo_editor_styles' );

f8cb74d0ddd2c0d9572085ae879a5da3ff94008b
Kjell Reigstad <kjell.reigstad@automattic.com>
2021-11-09 11:49:14 -0500

    Update functions.php
    Co-authored-by: Jeff Ong <jonger4@gmail.com>

--- functions.php
+++ functions.php
@@ -23,7 +23,7 @@ if ( ! function_exists( 'twentytwentytwo_support' ) ) :
 		add_theme_support( 'wp-block-styles' );
 
 		// Enqueue editor styles.
-		add_editor_style( 'style.css' );
+		add_editor_style( get_template_directory_uri() . '/style.css' );
 	}
 	add_action( 'after_setup_theme', 'twentytwentytwo_support' );
 

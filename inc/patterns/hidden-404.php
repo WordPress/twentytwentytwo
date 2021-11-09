d0370f42519af72debe2e49f364d1fd0ffc541db
Evan Herman <evan.m.herman@gmail.com>
2021-11-09 10:37:04 -0500

    Resovle PHPCS warnings

--- inc/patterns/hidden-404.php
+++ inc/patterns/hidden-404.php
@@ -3,9 +3,9 @@
  * 404 content.
  */
 return array(
-	'title'      => __( '404 content', 'twentytwentytwo' ),
-	'inserter'   => false,
-	'content'    => '<!-- wp:heading {"style":{"typography":{"fontSize":"clamp(4rem, 40vw, 20rem)","fontWeight":"200","lineHeight":"1"}},"className":"has-text-align-center"} -->
+	'title'    => __( '404 content', 'twentytwentytwo' ),
+	'inserter' => false,
+	'content'  => '<!-- wp:heading {"style":{"typography":{"fontSize":"clamp(4rem, 40vw, 20rem)","fontWeight":"200","lineHeight":"1"}},"className":"has-text-align-center"} -->
 					<h2 class="has-text-align-center" style="font-size:clamp(4rem, 40vw, 20rem);font-weight:200;line-height:1">' . esc_html( _x( '404', 'Error code for a webpage that is not found.', 'twentytwentytwo' ) ) . '</h2>
 					<!-- /wp:heading -->
 					<!-- wp:paragraph {"align":"center"} -->

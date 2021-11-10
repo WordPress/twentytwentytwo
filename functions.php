e28752c804fed1d2813b87502c3c169f38494998
Jon Desrosiers <desrosj@users.noreply.github.com>
2021-11-09 19:51:12 -0500

    Copy over changes made during commit to WP trunk.

--- functions.php
+++ functions.php
@@ -6,34 +6,38 @@
  *
  * @package WordPress
  * @subpackage Twenty_Twenty_Two
- * @since 1.0.0
+ * @since Twenty Twenty-Two 1.0
  */
 
 
 if ( ! function_exists( 'twentytwentytwo_support' ) ) :
 
 	/**
-	 * Sets up theme defaults and registers support for various WordPress features.
+	 * Add support for core block visual styles.
+	 * Styles load in both the editor and the front end.
+	 *
+	 * @link https://developer.wordpress.org/block-editor/how-to-guides/themes/theme-support/#default-block-styles
+	 *
+	 * @since Twenty Twenty-Two 1.0
 	 *
 	 * @return void
 	 */
 	function twentytwentytwo_support() {
 
-		// Add support for block styles.
 		add_theme_support( 'wp-block-styles' );
 
-		// Enqueue editor styles.
-		add_editor_style( get_template_directory_uri() . '/style.css' );
 	}
-	add_action( 'after_setup_theme', 'twentytwentytwo_support' );
 
 endif;
 
+add_action( 'after_setup_theme', 'twentytwentytwo_support' );
 
 if ( ! function_exists( 'twentytwentytwo_styles' ) ) :
 
 	/**
 	 * Enqueue styles.
+	 *
+	 * @since Twenty Twenty-Two 1.0
 	 *
 	 * @return void
 	 */
@@ -44,7 +48,7 @@ if ( ! function_exists( 'twentytwentytwo_styles' ) ) :
 			'twentytwentytwo-style',
 			get_template_directory_uri() . '/style.css',
 			array(),
-			wp_get_theme()->get( 'Version' ),
+			wp_get_theme()->get( 'Version' )
 		);
 
 		// Add styles inline.
@@ -54,15 +58,17 @@ if ( ! function_exists( 'twentytwentytwo_styles' ) ) :
 		wp_enqueue_style( 'twentytwentytwo-style' );
 
 	}
-	add_action( 'wp_enqueue_scripts', 'twentytwentytwo_styles' );
 
 endif;
 
+add_action( 'wp_enqueue_scripts', 'twentytwentytwo_styles' );
 
 if ( ! function_exists( 'twentytwentytwo_editor_styles' ) ) :
 
 	/**
 	 * Enqueue editor styles.
+	 *
+	 * @since Twenty Twenty-Two 1.0
 	 *
 	 * @return void
 	 */
@@ -72,9 +78,10 @@ if ( ! function_exists( 'twentytwentytwo_editor_styles' ) ) :
 		wp_add_inline_style( 'wp-block-library', twentytwentytwo_get_font_face_styles() );
 
 	}
-	add_action( 'admin_init', 'twentytwentytwo_editor_styles' );
 
 endif;
+
+add_action( 'admin_init', 'twentytwentytwo_editor_styles' );
 
 
 if ( ! function_exists( 'twentytwentytwo_get_font_face_styles' ) ) :
@@ -82,6 +89,8 @@ if ( ! function_exists( 'twentytwentytwo_get_font_face_styles' ) ) :
 	/**
 	 * Get font face styles.
 	 * Called by functions twentytwentytwo_styles() and twentytwentytwo_editor_styles() above.
+	 *
+	 * @since Twenty Twenty-Two 1.0
 	 *
 	 * @return string
 	 */
@@ -108,7 +117,6 @@ if ( ! function_exists( 'twentytwentytwo_get_font_face_styles' ) ) :
 	}
 
 endif;
-
 
 // Add block patterns
 require get_template_directory() . '/inc/block-patterns.php';

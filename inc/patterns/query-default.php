e9f49804eab30b51b9cd9b7d6b16674f80483b2c
Kjell Reigstad <kjell.reigstad@automattic.com>
2021-12-10 08:21:07 -0500

    Try using the default categories (#284)
    * Try using the default categories
    
    * Rename headers to header, footers to footer.
    
    * Add additional categories.
    
    * Move "General" patterns in to the  "Featured" category.
    
    * Rename query to posts, etc.
    
    * Use query category for now.
    
    * Update general-two-images-text.php

--- inc/patterns/query-default.php
+++ inc/patterns/query-default.php
@@ -4,7 +4,7 @@
  */
 return array(
 	'title'      => __( 'Default posts', 'twentytwentytwo' ),
-	'categories' => array( 'twentytwentytwo-query' ),
+	'categories' => array( 'query' ),
 	'blockTypes' => array( 'core/query' ),
 	'content'    => '<!-- wp:query {"query":{"perPage":10,"pages":0,"offset":0,"postType":"post","categoryIds":[],"tagIds":[],"order":"desc","orderBy":"date","author":"","search":"","exclude":[],"sticky":""},"align":"wide","layout":{"inherit":true}} -->
 					<div class="wp-block-query alignwide"><!-- wp:post-template {"align":"wide"} -->
